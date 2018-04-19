f657c9dc53a516485fc18b29ae36d470b30e5190
twilroad <269044570@qq.com>
2018-04-19 16:42:11 +0800

    chore(*): rebuild packages

--- package/commands/create.command.js
+++ package/commands/create.command.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class CreateCommand {
     async initialize(program) {
         program
-            .command("create", "")
+            .command("create", "Create a injection project for Notadd, which may be a addon, a extension, or a module.")
             .alias("c")
             .action(this.handle);
     }
--- package/commands/generate.command.js
+++ package/commands/generate.command.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class GenerateCommand {
     async initialize(program) {
         program
-            .command("generate", "")
+            .command("generate", "Generate a new project from Notadd.")
             .alias("g")
             .action(this.handle);
     }
