eb42f73e691ca819fccf8af952a3e48bdf61b7c1
twilroad <269044570@qq.com>
2018-04-19 16:41:43 +0800

    feat(command): add description for commands

--- src/commands/create.command.ts
+++ src/commands/create.command.ts
@@ -1,7 +1,7 @@
 export class CreateCommand {
     public async initialize(program: Caporal) {
         program
-            .command("create", "")
+            .command("create", "Create a injection project for Notadd, which may be a addon, a extension, or a module.")
             .alias("c")
             .action(this.handle);
     }
--- src/commands/generate.command.ts
+++ src/commands/generate.command.ts
@@ -1,7 +1,7 @@
 export class GenerateCommand {
     public async initialize(program: Caporal) {
         program
-            .command("generate", "")
+            .command("generate", "Generate a new project from Notadd.")
             .alias("g")
             .action(this.handle);
     }
